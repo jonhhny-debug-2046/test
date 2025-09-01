ee4fe6bafdacf60a5f7052d7a35ba16e1ed15a0a
jonhhny-debug-2046 <zhangtingyufound@gmail.com>
2025-09-01 16:23:48 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+import calculateSumOfNumbers3 from './test.js'
+
+const res = calculateSumOfNumbers3(1, 2)
+console.log('res: ', res)
+
+// import Class1 from './class1'
+
+// var class1 = new Class1()
+// class1.initialise()
